56cebd75c30c7bfcac415fb63dad3fdc17e40fe7
JuanGarcia745 <167032302+JuanGarcia745@users.noreply.github.com>
2024-10-30 19:34:49 -0500

    nonas

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.crearFormulario();
+  }
+
+  private crearFormulario() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(7)]]
+    });
+  }
+
+  public login() {
+    if (this.loginForm.valid) {
+      console.log("Datos del login:", this.loginForm.value);
+      // Aquí puedes agregar lógica adicional para manejar el envío del formulario
+      // como llamar a un servicio para autenticar al usuario
+    } else {
+      console.log("Formulario inválido");
+    }
+  }
 }
