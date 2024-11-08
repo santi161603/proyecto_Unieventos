2cdb19984d980d9cf06e8413bdc8a0501b5121d7
JuanGarcia745 <167032302+JuanGarcia745@users.noreply.github.com>
2024-11-08 17:15:49 -0500

    detalleEvento

--- src/app/componentes/detalle-evento/detalle-evento.component.ts
+++ src/app/componentes/detalle-evento/detalle-evento.component.ts
@@ -4,36 +4,28 @@ import { ActivatedRoute } from '@angular/router';
 import { EventosService } from '../../servicios/eventos.service';
 import { CommonModule } from '@angular/common';
 
-
 @Component({
- selector: 'app-detalle-evento',
- standalone: true,
- imports: [CommonModule],
- templateUrl: './detalle-evento.component.html',
- styleUrl: './detalle-evento.component.css'
+  selector: 'app-detalle-evento',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './detalle-evento.component.html',
+  styleUrls: ['./detalle-evento.component.css']
 })
 export class DetalleEventoComponent {
+  codigoEvento: string = '';
+  evento: EventoDTO | undefined;
 
+  constructor(private route: ActivatedRoute, private eventosService: EventosService) {
+    this.route.params.subscribe((params) => {
+      this.codigoEvento = params['id'];
+      this.obtenerEvento();
+    });
+  }
 
- codigoEvento: string = '';
- evento: EventoDTO | undefined;
-
-
- constructor(private route: ActivatedRoute, private eventosService: EventosService) {
-   this.route.params.subscribe((params) => {
-     this.codigoEvento = params['id'];
-     this.obtenerEvento();
-   });
- }
-
-
- public obtenerEvento() {
-   const eventoConsultado = this.eventosService.obtener(this.codigoEvento);
-   if (eventoConsultado != undefined) {
-     this.evento = eventoConsultado;
-   }
- }
-
-
+  public obtenerEvento() {
+    const eventoConsultado = this.eventosService.obtener(this.codigoEvento);
+    if (eventoConsultado != undefined) {
+      this.evento = eventoConsultado;
+    }
+  }
 }
-
