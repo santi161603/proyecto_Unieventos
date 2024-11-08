af7ea6db1bdd2f905d1be56611560ff9ba19733f
JuanGarcia745 <167032302+JuanGarcia745@users.noreply.github.com>
2024-11-08 18:49:53 -0500

    localidades

---
+++ src/app/componentes/gestionar-localidades/gestionar-localidades.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestionar-localidades',
+  standalone: true,
+  imports: [],
+  templateUrl: './gestionar-localidades.component.html',
+  styleUrl: './gestionar-localidades.component.css'
+})
+export class GestionarLocalidadesComponent {
+
+}
